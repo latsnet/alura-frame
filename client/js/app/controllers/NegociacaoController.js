09b81046c833f540dffbd93aa3d5136d6e06647f
Luiz Augusto Tavares da Silveira Mazzi <latsnet@gmail.com>
2020-02-29 15:42:16 -0300

    Refatoração do código - Criação de lista de negociações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,26 +6,29 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split("-").map((value, index) => value-(index%2)));
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limparCampos();
 
-        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
-
-        console.log(negociacao);
-
-        this.limparCampos();
+        console.log(this._listaNegociacoes.negociacoes);
     }
 
-    limparCampos() {
+    _criaNegociacao() {
+        return new Negociacao(DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
+    }
+
+    _limparCampos() {
 
         this._inputData.value = "";
         this._inputQuantidade.value = "1";
-        this._inputValor.value = "0";
+        this._inputValor.value = 0.0;
 
         this._inputData.focus();
     }
