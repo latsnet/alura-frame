69416ec7a4614c54924a52b367d3478903fbb283
Luiz Augusto Tavares da Silveira Mazzi <latsnet@gmail.com>
2020-03-01 22:49:35 -0300

    Atualização da view na alteração do modelo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,8 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+
         this._negociacoesView = new NegociacoesView($("#negociacoesView"));
         this._negociacoesView.update(this._listaNegociacoes);
 
@@ -21,10 +22,19 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = "Negociação adicionada com sucesso";
         this._mensagemView.update(this._mensagem);
-        this._negociacoesView.update(this._listaNegociacoes);
+
         this._limparCampos();
 
         console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    apaga(event) {
+
+        event.preventDefault();
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = "Negociações apagadas com sucesso";
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
