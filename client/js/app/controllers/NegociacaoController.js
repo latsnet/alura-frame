4def6c7008e245fdc21ffbec65c4e0e1aa87f3af
Luiz Augusto Tavares da Silveira <latsnet@gmail.com>
2021-02-12 22:23:18 -0300

    Ordenação das colunas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,8 +6,9 @@ class NegociacaoController {
         this._inputData = $("#data");this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
 
-        this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($("#negociacoesView")), 'adiciona', 'esvazia');
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($("#negociacoesView")), 'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
         this._mensagem = new Bind(new Mensagem(), new MensagemView($("#mensagemView")), 'texto');
+        this._ordemAtual = '';
     }
 
     adiciona(event) {
@@ -52,5 +53,14 @@ class NegociacaoController {
 
         this._inputData.focus();
     }
+
+    ordena(coluna) {
+        if (this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem();
+        } else {
+            this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+        }
+        this._ordemAtual = coluna;
+    }    
 
 }
